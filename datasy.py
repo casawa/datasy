8c7559b57f5a296168213da264c609d89f08605f
Chaitanya Asawa <casawa@stanford.edu>
2016-06-09 12:19:28 -0700

    Ignoring pyc files and starting more internal functions of main module

--- datasy.py
+++ datasy.py
@@ -21,6 +21,17 @@ class datasy:
         pass
 
     """
+    Removes punctuation for the data given
+    """
+    def __remove_punct(self, data):
+        no_punct = []
+        for example in data:
+            no_punct_example = [token.translate(None, string.punctuation) for token in example]
+            no_punct.append(no_punct_example)
+
+        data = no_punct
+
+    """
     Removes punctuation from the text data.
     """
     def remove_punctuation(self):
@@ -32,12 +43,34 @@ class datasy:
     def stem(self):
         pass
 
+    """
+    Removes stop words for the data given.
+    """
+    def __remove_stop(self, data):
+        #nonstop = []
+        #for example in data:
+        #    removed = 
+
+
+
+        data = nonstop
 
     """
     Removes stop words from the data.
     """
     def remove_stop_words(self):
-        pass
+       pass 
+
+    """
+    Lowercases the data given.
+    """
+    def __lower_data(self, data):
+        lowered = []
+        for example in data:
+            lowered_example = [token.lower() for token in example]
+            lowered.append(example)
+
+        data = lowered
 
     """
     Converts all the data into lowercase. 
